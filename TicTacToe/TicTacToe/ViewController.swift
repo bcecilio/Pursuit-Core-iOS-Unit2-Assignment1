77ab3419061f3be51ea539586039214e36f12225
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-05 15:48:55 -0500

    turn variables made / O's and X's set

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -11,17 +11,50 @@ import UIKit
 @available(iOS 13.0, *)
 class ViewController: UIViewController {
     
-    @IBOutlet var gameButtons: [GameButton]!
+    // player 1 is X, player 2 is O
+    var activePlayer = 1
     
-
+    @IBOutlet var gameButtons: [GameButton]!
+   
+    @IBOutlet weak var colRow0: GameButton!
+    @IBOutlet weak var colRow1: GameButton!
+    @IBOutlet weak var colRow2: GameButton!
+    @IBOutlet weak var colRow3: GameButton!
+    @IBOutlet weak var colRow4: GameButton!
+    @IBOutlet weak var colRow5: GameButton!
+    @IBOutlet weak var colRow6: GameButton!
+    @IBOutlet weak var colRow7: GameButton!
+    @IBOutlet weak var colRow8: GameButton!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        colRow0.setImage(UIImage(systemName: ""), for:.normal)
+        colRow1.setImage(UIImage(systemName: ""), for:.normal)
+        colRow2.setImage(UIImage(systemName: ""), for:.normal)
+        colRow3.setImage(UIImage(systemName: ""), for:.normal)
+        colRow4.setImage(UIImage(systemName: ""), for:.normal)
+        colRow5.setImage(UIImage(systemName: ""), for:.normal)
+        colRow6.setImage(UIImage(systemName: ""), for:.normal)
+        colRow7.setImage(UIImage(systemName: ""), for:.normal)
+        colRow8.setImage(UIImage(systemName: ""), for:.normal)
+    }
+    
     @IBAction func gameButtonPressed(_ gameButton: GameButton) {
         print("at row \(gameButton.row) at column \(gameButton.col)")
         
-        gameButton.setImage(UIImage(systemName: "xmark"), for:.normal)
+        if activePlayer == 1 {
+            gameButton.setImage(UIImage(systemName: "xmark"), for:.normal)
+            activePlayer = 2
+        } else {
+            gameButton.setImage(UIImage(systemName: "circle"), for:.normal)
+            activePlayer = 1
+        }
     }
     
 
